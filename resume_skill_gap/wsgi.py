361554222a9a33989e2d3ab6e1fcba11851d7bd3
Suraj Kanse <surajkanse88@gmail.com>
2026-08-01 22:52:23 +0530

    Expose app variable in wsgi.py for Vercel entry point

--- resume_skill_gap/wsgi.py
+++ resume_skill_gap/wsgi.py
@@ -3,3 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume_skill_gap.settings')
 application = get_wsgi_application()
+app = application
